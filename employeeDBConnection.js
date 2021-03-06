03610657c5862c2e4c3112742528f43f87b58253
Liz Town <liztown@gmail.com>
2021-03-06 11:59:01 -0600

    updating a few functions

--- employeeDBConnection.js
+++ employeeDBConnection.js
@@ -59,18 +59,42 @@ const init = () => {
 
 const viewAll = () => {
     // JOIN all 3 ugh
-    //  connection.query('SELECT * FROM ')
+      connection.query(`SELECT  e.id, e.first_name, e.last_name, m.first_name as managers_first_name, m.last_name as managers_last_name, r.title, r.salary, d.dept_name
+      FROM employeeDB.employee as e
+      LEFT JOIN employeeDB.employee as m
+      ON e.manager_id = m.id
+      JOIN role as r
+      ON e.role_id = r.id
+      JOIN department as d ON r.department_id = d.id`, (err, allRes) => {
+        if (err) throw err;
+       // console.log(allRes);
+        console.table(allRes)
+        init();
+
+
+      })
     // SUB QUERY INSIDE THE QUERY?
-    console.log("I'm sorry, this function is not available. Please try again.");
-    init();
+  //  console.log("I'm sorry, this function is not available. Please try again.");
+  //  init();
 };
 
 
 const viewDept = () => {
     // JOIN departments & employees
     // connection.query('SELECT ')
-    console.log("I'm sorry, this function is not available. Please try again.");
-    init();
+
+    connection.query(`SELECT e.id, e.first_name, e.last_name, r.title, d.dept_name FROM employee as e
+    LEFT JOIN role as r
+    ON e.role_id = r.id
+    JOIN department as d ON r.department_id = d.id`, (err, deptRes) => {
+        if (err) throw err;
+      // console.log(deptRes);
+        console.table(deptRes);
+        init();
+
+    })
+
+  //  console.log("I'm sorry, this function is not available. Please try again.");
 
 }
 
@@ -98,29 +122,40 @@ const viewMan = () => {
 }
 
 const addEmp = () => {
+    
+    let empArray = [];
+    let roleArray = [];
 
     let manList = ['none'];
     let roleList = ['other'];
 
-    connection.query('SELECT * FROM employee', (err, empRes) => {
+    connection.query(`SELECT id, last_name
+    FROM employeeDB.employee
+    WHERE manager_id IS NULL`, (err, empRes) => {
         if (err) throw err;
+        empRes.forEach(e => {
+            manList.unshift(e.last_name);
+            empArray.push(e);
+        })
         // if (empRes.manager_id != null) {
-        for (let i = 0; i < empRes.length; i++) {
-            if (empRes[i].id === empRes.manager_id) {
-                manList.unshift(`${empRes[i].last_name}`);
+        // for (let i = 0; i < empRes.length; i++) {
+        //     if (empRes[i].id === empRes.manager_id) {
+        //         manList.unshift(`${empRes[i].last_name}`);
 
-            }
-        }
-        //  }
+        //     }
+       
+         })
 
 
-        connection.query('SELECT title FROM role', (err, roleRes) => {
+        connection.query('SELECT id, title FROM role', (err, roleRes) => {
             if (err) throw err;
             roleRes.forEach(e => {
                 roleList.unshift(e.title);
+                roleArray.push(e);
+
             });
         })
-    })
+    
 
 
     inquirer.prompt([
@@ -149,7 +184,25 @@ const addEmp = () => {
 
     ]).then(addRes => {
         console.table(addRes);
-        init();
+        console.log(roleArray);
+        console.log(empArray[0].id);
+        // get ids and create employee object
+        // query - insert object 
+        let manObj = empArray.find(emp => addRes.manager_name === emp.last_name);
+        let roleObj = roleArray.find(role => addRes.role_name === role.title)
+
+        connection.query(`INSERT INTO employee
+        SET ?`, 
+        {
+            first_name: addRes.first_name,
+            last_name: addRes.last_name,
+            role_id: roleObj.id,
+            manager_id: manObj.id
+        }, (err => {
+            if (err) throw err;
+        }))
+
+       // init();
     })
 }
 
